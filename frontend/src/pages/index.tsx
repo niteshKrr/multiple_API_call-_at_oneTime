6e8ca187099a6cbdcd4c96ae74b05a12ed4ddedb
niteshKrr <nkumar35101@gmail.com>
2023-02-07 15:51:09 +0530

    minor change

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,24 +2,30 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import axios from "axios";
+import { useState } from "react";
+
+type MyApiType = {
+  data: string;
+};
 
 export default function Home() {
+  const [allresponse, setAllresponse] = useState<any>([]);
+
   const handleOnclick = async () => {
     const url1 = "http://localhost:4000/first";
     const url2 = "http://localhost:4000/second";
     const url3 = "http://localhost:4000/third";
     const url4 = "http://localhost:4000/fourth";
 
-    const [response1, response2, response3, response4] = await Promise.all([
+    const myResponse = await Promise.all([
       axios.get(url1),
       axios.get(url2),
       axios.get(url3),
       axios.get(url4),
     ]);
-    console.log("first response :",response1.data);
-    console.log("second response :",response2.data);
-    console.log("third response :",response3.data);
-    console.log("fourth response :",response4.data);
+
+    console.log("my responses are:", myResponse);
+    setAllresponse(myResponse);
   };
 
   return (
@@ -28,6 +34,14 @@ export default function Home() {
         <button onClick={handleOnclick}>
           Get all responses through multiple APIs
         </button>
+
+        <div style={{ marginTop: "30px" }}>
+          {allresponse.map((resp: MyApiType, idx: number) => (
+            <div key={idx}>
+              the response for api call {idx + 1} is :- {resp.data}
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
